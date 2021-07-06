6906681db9026daa3d731a77a98f49d2cb986d61
flavio <flavioliveira.7@gmail.com>
2021-07-07 00:17:57 +0200

    understanding recursivity

---
+++ recursive.py
@@ -0,0 +1,11 @@
+def factorial(x):
+  """recursive function to find the factorial of an integer"""
+
+  if x == 1:
+    return 1
+  else:
+    return (x * factorial(x-1))
+
+num = 3
+
+print(f"Factorial of {num}, is {factorial(num)}")
