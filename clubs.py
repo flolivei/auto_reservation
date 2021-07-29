6796921555ea7554122f75648324b01d533cd82a
flavio <flavioliveira.7@gmail.com>
2021-07-29 23:55:43 +0200

    added functions to connect and insert SQL

--- clubs.py
+++ clubs.py
@@ -1,4 +1,5 @@
 from re import I
+from sqlite3.dbapi2 import Error
 from selenium.webdriver import Chrome
 from bs4 import BeautifulSoup
 from selenium.webdriver.support.wait import WebDriverWait
@@ -9,6 +10,35 @@ import datetime
 import time
 import unidecode
 import sqlite3
+
+def create_connection(db_file):
+    """ create a database connection to the SQLite database 
+        specified by db_file 
+    :param db_file: database file
+    :retur: connection object or None
+    """
+    conn = None
+    try: 
+        conn = sqlite3.connect(db_file)
+        print(sqlite3.version)
+        return conn
+    except Error as e:
+        print(e)
+    
+    return conn
+
+def create_club(conn, club):
+    """ create a new club into the clubs table
+    :param conn:
+    :param club:
+    :return: club id
+    """
+    sql = '''INSERT INTO club (id, name) VALUES (?, ?)'''
+
+    cursor = conn.cursor()
+    cursor.execute(sql, club)
+    conn.commit()
+    return cursor.lastrowid
 
 driver = Chrome(executable_path='C:\\Users\\flolivei\\Documents\\chromedriver.exe')
 driver.get("https://www.aircourts.com/index.php/")
@@ -25,23 +55,23 @@ for sport_extract in sports_extract:
         'sport' : sport_extract.get_text()}
   print(sport)
 
-try:
-    sqliteConnection = sqlite3.connect('sports_freq.db')
-    cursor = sqliteConnection.cursor()
-    print("Database created and Successfully Connected to SQLite")
 
-    sqlite_select_Query = "select sqlite_version();"
-    cursor.execute(sqlite_select_Query)
-    record = cursor.fetchall()
-    print("SQLite Database Version is: ", record)
-    cursor.close()
-s
-except sqlite3.Error as error:
-    print("Error while connecting to sqlite", error)
-finally:
-    if sqliteConnection:
-        sqliteConnection.close()
-        print("The SQLite connection is closed")
+database = r"C:\Users\flolivei\Documents\GitHub\auto_reservation\sports_freq.db"
+
+# creates a database connection
+conn = create_connection(database)
+
+with conn:
+    for sport_extract in sports_extract:
+        club = (sport_extract["value"], sport_extract.get_text())
+        id = create_club(conn, club)
+        print(id)
+
+if conn: 
+    conn.close()
+    print("The SQLite connection is closed")
+
+
 #print(locations)
 
 # selects the wanted date
