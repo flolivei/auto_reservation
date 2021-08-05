f3e629d152a96db195b2472f4ceae4a9663f3456
flavio <flavioliveira.7@gmail.com>
2021-08-05 23:45:35 +0200

    clubs table implemented with insert

--- exp.py
+++ exp.py
@@ -6,3 +6,10 @@ date1 = datetime.datetime.utcnow()
 d = datetime.datetime(2019, 2, 4).today()
 time = f'{d.hour}:{d.minute}:{d.microsecond}'
 print(time)
+
+soup = BeautifulSoup(page_source, 'lxml')
+clubs = soup.find("div", class_ = "court_container").find_all("div", class_ = "club-container") # list of padel clubs in "Grande Lisboa"
+
+for club in clubs: 
+    name = club.find('div', class_ = 'club-info').h2.text
+    print(name)
